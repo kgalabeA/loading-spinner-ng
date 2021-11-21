aacaecb83ca824332e0422eea28f5469df6dc930
kgalabeA <albert.mogodi@falcorp.co.za>
2021-11-21 12:46:30 +0200

    spinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'loading-spinner-ng';
+  isLoading!: boolean;
+
+  constructor(private spinner: NgxSpinnerService) { }
+
+  ngOnInit() {
+    // this.spinner.show();
+    // setTimeout(() => {
+    //   this.spinner.hide();
+    // }, 2000);
+  }
+
+  showSpinner() {
+    this.spinner.show();
+    //this.isLoading=true;
+    setTimeout(() => {
+      this.spinner.hide();
+     //this.isLoading=false;
+    }, 3000);
+  }
+
+  showSpinner1() {
+    //this.spinner.show();
+    this.isLoading=true;
+    setTimeout(() => {
+     // this.spinner.hide();
+     this.isLoading=false;
+    }, 3000);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import {
+  NbThemeModule,
+  NbLayoutModule,
+  NbButtonModule
+} from '@nebular/theme';
 
 import { AppComponent } from './app.component';
+import { SpinnerComponent } from './spinner/spinner.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+const routes: Routes = [];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes, { useHash: true }),
+    NgxSpinnerModule,
+    NbThemeModule.forRoot(),
+    NbLayoutModule,
+    NbButtonModule,
+    BrowserAnimationsModule,
+    NgbModule,
   ],
-  providers: [],
+  declarations: [AppComponent, SpinnerComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
